ab223df70fe0928da0ea973e007c241b8ec73c41
“AncaBirisan <ankutza_peace@yahoo.com>
2020-12-09 16:39:12 +0200

    Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,14 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello Anca");
+        System.out.println(5+5);
+        System.out.println(25/5);
+        System.out.println(-5 + 8 * 6);
+        System.out.println((55+9) % 9);
+        System.out.println (20 + -3*5 / 8 );
+        System.out.println(5 + 15 / 3 * 2 - 8 % 3);
+
+
 
     }
 }
